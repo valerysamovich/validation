f517d3cb4791c862550b79baff07d2ea561bae83
Valery Samovich <valerysamovich@gmail.com>
2014-11-27 23:27:29 -0500

    Sanity fix

--- validation.js
+++ validation.js
@@ -1,48 +1,42 @@
 // JavaScript Document
 <!-- 
-  function validateForm()
-{
+  function validateForm() {
+	
 	// variables declarations
 	var name = document.form.name.value;
 	var comments = document.form.comments.value;
 	
 	// if-else conditional statement for name
-	if(name == "")
-	{
+	if(name == "") {
 		alert("Please fill in the name field!");
 		document.form.name.focus();
 		return false;
 	} 	
 	
 	// if-else conditional statement for gender
-	if(!document.form.gender[0].checked && !document.form.gender[1].checked)
-	{
+	if(!document.form.gender[0].checked && !document.form.gender[1].checked) {
 		alert("Plase ckeck your gender!");
 		return false;
 	}
 	
 	// if-else conditional statement for interest
-	if(!document.form.interest[0].checked && !document.form.interest[1].checked && !document.form.interest[2].checked)
-	{
+	if(!document.form.interest[0].checked && !document.form.interest[1].checked && !document.form.interest[2].checked) {
 		alert("Please check at least one interest!");
 		return false;
 	}
 	
 	// if-else conditional statement for age
-	if(document.form.age[document.form.age.selectedIndex].value == "")
-    {
-    	alert ("Please select your age!");
+	if(document.form.age[document.form.age.selectedIndex].value == "") {
+    		alert ("Please select your age!");
 		return false;
 	}
 	
 	// if-else conditional statement for comments
-	if(comments == "")
-	{
+	if(comments == "") {
 		alert("Please fill in the comments field!");
 		document.form.comments.focus();
 		return false;
-	}else
-	{
+	} else {
 		return true;
 	}
 }
